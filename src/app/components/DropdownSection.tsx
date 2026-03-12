8b81e4614b9d5004286b47496cc883e31f446b36
JoaoFlavio11 <joaoflaviocl@gmail.com>
2026-03-11 21:46:33 -0300

    .

---
+++ src/app/components/DropdownSection.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useState } from "react";
+
+export default function DropdownSection() {
+  const [openSection, setOpenSection] = useState({
+    
+  });
+
+  return(
+
+  );
+}
