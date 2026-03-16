686355daf867940808a1710b4bd75f8ae47c4025
João Flávio C. Lopes <joaoflaviocl@gmail.com>
2026-03-15 23:44:21 -0300

    .

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,6 +1,37 @@
-import { projetos } from "@/api/projects";
+
 import { Cloud, Code2, ExternalLink, Terminal } from "lucide-react";
 import DropdownSection from "../layout/DropdownSection";
+import type { Projeto } from "@/types/types";
+
+export const projetos: Projeto[] = [
+  {
+    id: 1,
+    titulo: "Warehouse API",
+    descricao:
+      "API de um sistema WMS (Warehouse Management System) desenvolvido em Python utilizando a teoria de Grafos para otimização de rotas e armazenamento.",
+    link: "https://github.com/JoaoFlavio11/warehouse-api",
+    tags: ["Python", "Grafos", "Django","REST"],
+    icone: "terminal",
+  },
+  {
+    id: 1,
+    titulo: "Easyroute WMS",
+    descricao:
+      "Frontend dedicado ao projeto Warehous API",
+    link: "https://github.com/JoaoFlavio11/data-warehouse",
+    tags: ["NextJS", "NodeJS", "Typescript","TailwindCSS"],
+    icone: "code",
+  },
+  {
+    id: 3,
+    titulo: "Cloud Infrastructure",
+    descricao:
+      "Repositórios e automações focados em provisionamento de infraestrutura como código (IaC), pipelines de CI/CD e orquestração de containers.",
+    link: "https://github.com/JoaoFlavio11",
+    tags: ["Docker", "AWS", "CI/CD"],
+    icone: "cloud",
+  },
+];
 
 export default function ProjectsSection() {
   const renderIcon = (iconName: string) => {
